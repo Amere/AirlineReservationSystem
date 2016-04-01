6879e49c41d60307d87057fdd5328ae19f2643f4
Ahmed Amer <ahmedamerxx@gmail.com>
2016-04-01 16:42:20 +0200

    all directives and html for seats done

--- public/js/controllers/reservCtrl.js
+++ public/js/controllers/reservCtrl.js
@@ -3,7 +3,7 @@ lufthansa.controller('reservCtrl', function($scope, lufthansaServ) {
   lufthansaServ.getReservDummy().success(function(flight) {
        $scope.flight = flight[0];
        $scope.aircraft = flight[0].aircraft;
-       $scope.economeySeats = flight[0].aircraft.economeySeats;
+       $scope.economySeats = flight[0].aircraft.economeySeats;
        $scope.premiumEconomySeats = flight[0].aircraft.premiumEconomySeats;
        $scope.businessSeats = flight[0].aircraft.businessSeats;
        $scope.firstClassSeats = flight[0].aircraft.firstClassSeats;
--- public/js/directives/seatMap.js
+++ public/js/directives/seatMap.js
@@ -1,7 +1,7 @@
 lufthansa.directive('seat',function(){
   var directive = {};
   directive.restrict = 'E';
-  directive.template = "<div ng-repeat='plane in planes'><div ng-repeat='seat in plane.seats'><h1>{{seat.placeCode}}</h1></div></div>";
-  directive.scope = { planes: "=seatN"};
+//  directive.template ="here is a seat";
+  directive.scope = { seat: "=seat"};
   return directive;
 })
