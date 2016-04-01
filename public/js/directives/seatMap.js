1303ba3c9624fe0d76bc07abaa1a896eece28edd
Ahmed Amer <ahmedamerxx@gmail.com>
2016-04-01 21:33:50 +0200

    the structure of each class seat directive done, missing on-click for each seat

--- public/js/directives/seatMap.js
+++ public/js/directives/seatMap.js
@@ -1,7 +1,7 @@
 lufthansa.directive('seat',function(){
   var directive = {};
   directive.restrict = 'E';
-//  directive.template ="here is a seat";
-  directive.scope = { seat: "=seat"};
+  directive.template ="<img src='images/economeySeat.png'>";
+  directive.scope = { seat: "=seat",imagesrc:"=src_"};
   return directive;
 })
