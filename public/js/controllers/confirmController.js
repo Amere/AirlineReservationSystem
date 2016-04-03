47f1bbca0c3834bf1949dcc0e62610c6c38e0f23
Nourhan510 <nourhan.nader@student.guc.edu.eg>
2016-04-03 11:03:32 +0200

    done

---
+++ public/js/controllers/confirmController.js
@@ -0,0 +1,9 @@
+
+lufthansa.controller('confirmController', function($scope, lufthansaServ) {
+  lufthansaServ.getConfirmDummy().success(function(dummy) {
+       $scope.Confirm = dummy;
+   });
+});
+
+
+  
