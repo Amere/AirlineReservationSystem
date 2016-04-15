bf3d2027dcd91027ac11db2b3a6752e985243c97
Ahmed Sawahel <swahel94@gmail.com>
2016-04-15 23:11:48 +0200

    still testing config

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var users = require('./routes/users');
 
 var app = express();
 require('dotenv').load();
-var doNothing = 8;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
