2c85a92664b0c3375424c09a2ed74df8c9522b7b
Hossam <hossam.elfar95@gmail.com>
2016-04-17 00:46:29 +0200

    adding comments to index.js ,lufthansaServ.js ,flights.js and db.js

--- public/Models/flights.js
+++ public/Models/flights.js
@@ -1,5 +1,8 @@
 var con = require('../../db');
-
+/**
+ * Seeding the DB from JSON files
+ * @returns void
+ */
 exports.seed=function(cb) {
   con.db().collection('aircrafts').find({}).toArray(function(err,docs){
     if(docs.length==0){
@@ -16,7 +19,10 @@ exports.seed=function(cb) {
   }
 });
 };
-
+/**
+ * Retrieve All flights from DB
+ * @returns {Array}
+ */
 function getAllFlightsFromDB(cb) {
   var data =con.db().collection('flights').find( ).toArray(function (err,flights) {
   if (flights.length==0) {
@@ -29,8 +35,15 @@ function getAllFlightsFromDB(cb) {
 };
 
 
-//Search Round Trip for app.get Here
-
+/**
+ * ROUND-TRIP SEARCH From DB
+ * @param origin - Flight Origin Location - Airport Code
+ * @param destination - Flight Destination Location - Airport Code
+ * @param departingDate - JavaScript Date.GetTime() numerical value corresponding to format `YYYY-MM-DD`
+ * @param returningDate - JavaScript Date.GetTime() numerical value corresponding to format `YYYY-MM-DD`
+ * @param class - economy or business only
+ * @returns {Array}
+ */
 function getRoundTrip(origin,destination,departingDate,returningDate,db,cb) {
   var data =con.db().collection('flights').find( { "origin": origin , "destination" : destination,"departingDate" : departingDate,"returningDate" : returningDate}).toArray(function (err,fli) {
   if (fli.length==0) {
@@ -41,31 +54,30 @@ function getRoundTrip(origin,destination,departingDate,returningDate,db,cb) {
   });
 
 };
-
-
-//Search Round Trip for app.post Here
-function postRoundTrip() {
-
-
-};
-
-
-
-
-//Search One way for app.get Here
-function getOneWay() {
-
-};
-
-
-
-
-//Search One way for app.post Here
-function PostOneWay() {
-
-
+/**
+ * ONE-WAY SEARCH From DB
+ * @param origin - Flight Origin Location - Airport Code
+ * @param DepartingDate - JavaScript Date.GetTime() numerical value corresponding to format `YYYY-MM-DD`
+ * @param class - economy or business only
+ * @returns {Array}
+ */
+function getOneWayTrip(origin,destination,departingDate,db,cb) {
+  var data =con.db().collection('flights').find( { "origin": origin , "destination" : destination,"departingDate" : departingDate}).toArray(function (err,fli) {
+    if (fli.length==0) {
+      cb(err,fli);
+    }  else {
+      cb(null,fli);
+    }
+  });
 
 };
+
+
+
+
+
+
+
 
 exports.getAllFlightsFromDB=getAllFlightsFromDB;
 exports.getRoundTrip=getRoundTrip;
