0c29f305991a8becd9c972b32da56fe5133e70bb
Hossam <hossam.elfar95@gmail.com>
2016-04-17 03:26:30 +0200

    edit some typo in documentation of index.js

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,12 @@ var router = express.Router();
 /**
  * Jwt package for json web token
  */
-var jwt     = require('jsonwebtoken');
-var flights=require('../public/Models/flights.js');
+var jwt = require('jsonwebtoken');
+var flights = require('../public/Models/flights.js');
 /**
  * Our DB connection
  */
-var db=require('../db.js');
+var db = require('../db.js');
 /**
  * moment package timing manipulation
  */
@@ -22,91 +22,89 @@ var moment = require('moment');
 /**
  * Seeding our database
  */
-db.connect (function(err,db){
-  flights.seed(function(err,seeded){
-    if(err) throw err;
-    if(!seeded){
-      console.log('data already inserted before');
-    }else{
-      console.log('data  inserted ');
+db.connect(function (err, db) {
+    flights.seed(function (err, seeded) {
+        if (err) throw err;
+        if (!seeded) {
+            console.log('data already inserted before');
+        } else {
+            console.log('data  inserted ');
 
-    }
-  })
+        }
+    })
 });
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('landing');
+router.get('/', function (req, res, next) {
+    res.render('landing');
 });
 /* GET google maps API */
-router.get('/google7a607af0cf3cce8e.html', function(req, res, next) {
-  res.render('google7a607af0cf3cce8e.html');
+router.get('/google7a607af0cf3cce8e.html', function (req, res, next) {
+    res.render('google7a607af0cf3cce8e.html');
 });
-
-
 
 
 /* GET airports codes */
-router.get('/api/data/codes',function(req,res){
-  var codes =  require('../airports.json');
-  res.json( codes );
+router.get('/api/data/codes', function (req, res) {
+    var codes = require('../airports.json');
+    res.json(codes);
 });
 
 /* GET offers */
-router.get('/api/data/offers',function(req,res){
-  var offers =  require('../offers.json');
-  res.json( offers );
+router.get('/api/data/offers', function (req, res) {
+    var offers = require('../offers.json');
+    res.json(offers);
 });
 
 /* GET news */
-router.get('/api/data/news',function(req,res){
-  var news =  require('../news.json');
-  res.json( news );
+router.get('/api/data/news', function (req, res) {
+    var news = require('../news.json');
+    res.json(news);
 });
-router.get('/api/data/flight',function(req,res){
-  var dummy =  require('../flight.json');
-  res.json( dummy );
+router.get('/api/data/flight', function (req, res) {
+    var dummy = require('../flight.json');
+    res.json(dummy);
 });
 
 /* GET slides */
-router.get('/api/data/slides',function(req,res){
-  var slides =  require('../slides.json');
-  res.json( slides );
+router.get('/api/data/slides', function (req, res) {
+    var slides = require('../slides.json');
+    res.json(slides);
 });
-router.get('/api/data/bookings',function(req,res){
-  var bookings =  require('../bookings.json');
-  res.json( bookings );
+router.get('/api/data/bookings', function (req, res) {
+    var bookings = require('../bookings.json');
+    res.json(bookings);
 });
-router.get('/api/data/pastFlights',function(req,res){
-  var pastFlights =  require('../pastFlights.json');
-  res.json( pastFlights );
+router.get('/api/data/pastFlights', function (req, res) {
+    var pastFlights = require('../pastFlights.json');
+    res.json(pastFlights);
 });
-router.get('/api/data/bookings',function(req,res){
-  var pastFlights =  require('../bookings.json');
-  res.json( pastFlights );
+router.get('/api/data/bookings', function (req, res) {
+    var pastFlights = require('../bookings.json');
+    res.json(pastFlights);
 });
 /**
- * Nationality REST ENDPOINT
+ * Nationalities REST ENDPOINT
  * @returns {Array}
  */
-router.get('/api/data/nations',function(req,res){
-  var nat =  require('../nationalities.json');
-  res.json( nat );
+router.get('/api/data/nations', function (req, res) {
+    var nat = require('../nationalities.json');
+    res.json(nat);
 });
 /**
  * Aircraft REST ENDPOINT
  * @param name - Aircraft name
  * @returns {Array}
  */
-router.get('/api/data/aircraft/:name',function(req,res){
-  db.db().collection('aircrafts').findOne({name:req.params.name},function(err,data){
-    if(err){
-      console.log('error in retrieving aircraft');
-    }else{
-    res.json(data);
-  }
-  });
+router.get('/api/data/aircraft/:name', function (req, res) {
+    db.db().collection('aircrafts').findOne({name: req.params.name}, function (err, data) {
+        if (err) {
+            console.log('error in retrieving aircraft');
+        } else {
+            res.json(data);
+        }
+    });
 });
 
 // router.use(function(req, res, next) {
@@ -129,9 +127,9 @@ router.get('/api/data/aircraft/:name',function(req,res){
 // });
 
 
-router.get('/api/data/conf',function(req,res){
-  var dummy =  require('../confirm.json');
-  res.json( dummy );
+router.get('/api/data/conf', function (req, res) {
+    var dummy = require('../confirm.json');
+    res.json(dummy);
 });
 
 /**
@@ -144,17 +142,17 @@ router.get('/api/data/conf',function(req,res){
  * @returns {Array}
  */
 
-router.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate', function(req, res) {
-  var origin =req.params.origin;
-  var destination=req.params.destination;
-  var departingDate=req.params.departingDate;
-  var returningDate=req.params.returningDate;
-  var x=moment(departingDate).toDate().getTime();
-  var y=moment(returningDate).toDate().getTime();
-  //var clas=req.params.class;
-  flights.getRoundTrip(origin,destination,x,y,db,function(err,result) {
-     res.json(result);
-   });
+router.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate', function (req, res) {
+    var origin = req.params.origin;
+    var destination = req.params.destination;
+    var departingDate = req.params.departingDate;
+    var returningDate = req.params.returningDate;
+    var x = moment(departingDate).toDate().getTime();
+    var y = moment(returningDate).toDate().getTime();
+    //var clas=req.params.class;
+    flights.getRoundTrip(origin, destination, x, y, db, function (err, result) {
+        res.json(result);
+    });
 });
 
 /**
@@ -164,23 +162,23 @@ router.get('/api/flights/search/:origin/:destination/:departingDate/:returningDa
  * @param class - economy or business only
  * @returns {Array}
  */
-router.get('/api/flights/search/:origin/:destination/:departingDate/:class', function(req, res) {
-  var origin =req.params.origin;
-  var destination=req.params.destination;
-  var departingDate=req.params.departingDate;
-  var x=moment(departingDate).toDate().getTime();
-  flights.getOneWayTrip(origin,destination,x,db,function(err,result) {
-    res.json(result);
-  });
+router.get('/api/flights/search/:origin/:destination/:departingDate/:class', function (req, res) {
+    var origin = req.params.origin;
+    var destination = req.params.destination;
+    var departingDate = req.params.departingDate;
+    var x = moment(departingDate).toDate().getTime();
+    flights.getOneWayTrip(origin, destination, x, db, function (err, result) {
+        res.json(result);
+    });
 });
 /**
- * All Fights ENDPOINT
+ * All Flights ENDPOINT
  * @returns {Array}
  */
-router.get('/api/all',function (req,res) {
-  flights.getAllFlightsFromDB(function (err,result) {
-    res.json(result);
-  });
+router.get('/api/all', function (req, res) {
+    flights.getAllFlightsFromDB(function (err, result) {
+        res.json(result);
+    });
 });
 
 module.exports = router;
