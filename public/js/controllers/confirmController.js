d5911e0d3dbb17fa011b4ac59f0026486b7c7e71
Hossam <hossam.elfar95@gmail.com>
2016-04-19 23:38:58 +0200

    merging

--- public/js/controllers/confirmController.js
+++ public/js/controllers/confirmController.js
@@ -17,37 +17,54 @@ if(lufthansaServ.paymentFlag === true){
 
     $scope.flg = lufthansaServ.getReturning_Or_Outgoing();
     if($scope.flg=="Returning"){
-      $scope.rr=true;
-      $scope.rSeat=lufthansaServ.getSeat();
-      $scope.Confirm.seatCodeOut= lufthansaServ.getSeatR();
-      $scope.Confirm.seatClass= lufthansaServ.getSeatClass_();
-      $scope.Confirm.flight=lufthansaServ.getFlightNumberOutGoing();
-      $scope.Confirm.odate= lufthansaServ.getDateOutGoing();
-      user.odate=lufthansaServ.getDateOutGoing();
-      user.flight=lufthansaServ.getFlightNumberOutGoing();
-      user.returnFlight=lufthansaServ.getFlightNumberReturning();
-      $scope.rFlight=user.returnFlight;
-      user.seatCode=lufthansaServ.getSeatR();
-      user.seatReturn=lufthansaServ.getSeat();
-      user.seatClass= lufthansaServ.getSeatClass_();
-      lufthansaServ.reserveSeat(lufthansaServ.getFlightNumberOutGoing(),$scope.Confirm.seatCode);
-      lufthansaServ.reserveSeat(lufthansaServ.getFlightNumberReturning(),$scope.Confirm.seatCode);
-      lufthansaServ.addUser(user,function(res){
-        $scope.receipt=res;
-      });
+      if(lufthansaServ.getSeat()) {
+        $scope.rr = true;
+        $scope.rSeat = lufthansaServ.getSeat();
+        $scope.Confirm.seatCodeOut = lufthansaServ.getSeatR();
+        $scope.Confirm.seatClass = lufthansaServ.getSeatClass_();
+        $scope.Confirm.flight = lufthansaServ.getFlightNumberOutGoing();
+        $scope.Confirm.odate = lufthansaServ.getDateOutGoing();
+        user.odate = lufthansaServ.getDateOutGoing();
+        user.flight = lufthansaServ.getFlightNumberOutGoing();
+        user.returnFlight = lufthansaServ.getFlightNumberReturning();
+        $scope.rFlight = user.returnFlight;
+        user.seatCode = lufthansaServ.getSeatR();
+        user.seatReturn = lufthansaServ.getSeat();
+        user.seatClass = lufthansaServ.getSeatClass_();
+        if (lufthansaServ.getSeat())
+          lufthansaServ.reserveSeat(lufthansaServ.getFlightNumberOutGoing(), $scope.Confirm.seatCode);
+        lufthansaServ.reserveSeat(lufthansaServ.getFlightNumberReturning(), $scope.Confirm.seatCode);
+        lufthansaServ.addUser(user, function (res) {
+          $scope.receipt = res;
+        });
+      }else{
+        $scope.Confirm.flight = lufthansaServ.getFlightNumberOutGoing();
+        $scope.Confirm.odate = lufthansaServ.getDateOutGoing();
+        user.odate = lufthansaServ.getDateOutGoing();
+        user.flight = lufthansaServ.getFlightNumberOutGoing();
+        user.returnFlight = lufthansaServ.getFlightNumberReturning();
+        $scope.rFlight = user.returnFlight;
+      }
     }else{
-      $scope.Confirm.seatCodeOut= lufthansaServ.getSeat();
-      $scope.Confirm.seatClass= lufthansaServ.getSeatClass_();
-      $scope.Confirm.flight=lufthansaServ.getFlightNumberOutGoing();
-      $scope.Confirm.odate= lufthansaServ.getDateOutGoing();
-      user.odate=lufthansaServ.getDateOutGoing();
-      user.flight=lufthansaServ.getFlightNumberOutGoing();
-      user.seatCode=lufthansaServ.getSeat();
-      user.seatClass= lufthansaServ.getSeatClass_();
-      lufthansaServ.reserveSeat(lufthansaServ.getFlightNumberOutGoing(),$scope.Confirm.seatCode);
-      lufthansaServ.addUser(user,function(res){
-        $scope.receipt=res;
-      });
+      if(lufthansaServ.getSeat()) {
+        $scope.Confirm.seatCodeOut = lufthansaServ.getSeat();
+        $scope.Confirm.seatClass = lufthansaServ.getSeatClass_();
+        $scope.Confirm.flight = lufthansaServ.getFlightNumberOutGoing();
+        $scope.Confirm.odate = lufthansaServ.getDateOutGoing();
+        user.odate = lufthansaServ.getDateOutGoing();
+        user.flight = lufthansaServ.getFlightNumberOutGoing();
+        user.seatCode = lufthansaServ.getSeat();
+        user.seatClass = lufthansaServ.getSeatClass_();
+        lufthansaServ.reserveSeat(lufthansaServ.getFlightNumberOutGoing(), $scope.Confirm.seatCode);
+        lufthansaServ.addUser(user, function (res) {
+          $scope.receipt = res;
+        });
+      }else{
+        $scope.Confirm.flight = lufthansaServ.getFlightNumberOutGoing();
+        $scope.Confirm.odate = lufthansaServ.getDateOutGoing();
+        user.odate = lufthansaServ.getDateOutGoing();
+        user.flight = lufthansaServ.getFlightNumberOutGoing();
+      }
     }
 
 
