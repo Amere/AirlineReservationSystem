cd230c2bfa6d334e0f80df9991fd398f9a0baad5
Ahmed Amer <ahmedamerxx@gmail.com>
2016-04-25 22:16:49 +0200

    Ionic structure integrated in our app, routing handled html pages for partials done, handling the server side done in ionic and recieving info from APIs in ionic using X-Origin done and added to our app.js, myApp have everything

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req,res,next){
+  res.append('Access-Control-Allow-Origin',req.headers.origin || '*');
+  res.append('Access-Control-Allow-Credentials','true');
+  res.append('Access-Control-Allow-Methods',['GET','OPTIONS','PUT','POST']);
+  res.append('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
