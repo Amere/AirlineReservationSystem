1d04072cb26f9139d7506cc7763ad5030e4cda3a
Ahmed Amer <ahmedamerxx@gmail.com>
2016-04-28 19:15:46 +0200

    added some animation to the app

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -45,7 +45,8 @@ lufthansa.config(function($stateProvider, $urlRouterProvider) {
   // Each state's controller can be found in controllers.js
   $stateProvider
   // setup an abstract state for the tabs directive
-    .state('tab', {
+
+  .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
