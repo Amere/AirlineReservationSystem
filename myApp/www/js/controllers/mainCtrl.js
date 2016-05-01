e6fae85602286a3be7f06bca8c8f054b9eb76324
Ahmed Amer <ahmedamerxx@gmail.com>
2016-05-02 01:19:12 +0200

    Added alot of difference to the app design, still working on it

--- myApp/www/js/controllers/mainCtrl.js
+++ myApp/www/js/controllers/mainCtrl.js
@@ -1,10 +1,23 @@
 /**
  * Our main Controller
  **/
-lufthansa.controller('mainCtrl', function ($scope, lufthansaServ , $document, $log,$state, $ionicPopup, $ionicLoading,$http) {
+lufthansa.controller('mainCtrl', function ($scope, lufthansaServ , $document, $log,$state, $ionicPopup, $ionicLoading,$http,$ionicTabsDelegate) {
     /*----------- Angular Bootstrap Datepicker -----------*/
     $scope.goToLanding= function(){
             $state.go('tab.landing');
+    };
+    $scope.goForward = function () {
+        var selected = $ionicTabsDelegate.selectedIndex();
+        if (selected != -1) {
+            $ionicTabsDelegate.select(selected + 1);
+        }
+    };
+
+    $scope.goBack = function () {
+        var selected = $ionicTabsDelegate.selectedIndex();
+        if (selected != -1 && selected != 0) {
+            $ionicTabsDelegate.select(selected - 1);
+        }
     };
     lufthansaServ.getAirportCodes().success(function (airports) {
              $scope.Airports = airports;
