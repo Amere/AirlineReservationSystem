eb34c68f9f408df9f7b3074b1e8ae6fc830da482
Ahmed Amer <ahmedamerxx@gmail.com>
2016-05-02 16:48:17 +0200

    animation added to the info page

--- myApp/www/js/controllers/paymentCtrl.js
+++ myApp/www/js/controllers/paymentCtrl.js
@@ -48,7 +48,7 @@ lufthansa.controller('paymentCtrl',function($scope,lufthansaServ,$state, $ionicP
     var cardNumber = $scope.card.v1;
     var cvv = $scope.card.ccv;
     var exp = $scope.card.validThru;
-    var other = true;
+    var other = false;
     var flagPK = false ;
     if(other==true){
       lufthansaServ.getPK().success(function(data){
